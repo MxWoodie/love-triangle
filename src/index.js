bac94e2603b4d9b92759233d40130f1175378f57
mxwoodie <maxswation@gmail.com>
2018-09-19 14:29:28 +0300

    Counting love triangles

--- src/index.js
+++ src/index.js
@@ -4,4 +4,23 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let count = 0;
+
+  for(let i=0;i<preferences.length;i++){
+
+    let firstPrefer = preferences[i];
+    let secondPrefer = preferences[firstPrefer-1];
+    let thirdPrefer = preferences[secondPrefer-1];
+
+    if(i === thirdPrefer-1 && firstPrefer !== secondPrefer && secondPrefer !== thirdPrefer && thirdPrefer !== firstPrefer){
+      count++;
+    }
+
+
+   /* if(preferences[i]+1 === preferences[i+1] && preferences[i+1]-2 === preferences[i+2] && preferences[i+2]+1 === preferences[i]){
+      count++;
+    }*/
+  }
+
+  return count/3;
 };
